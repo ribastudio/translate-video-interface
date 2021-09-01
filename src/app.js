fcfc7caec17e9943dfa416ae7a6b7355b9aa7e13
˜Riba Jr <ribastudio@gmail.com>
2021-08-31 23:43:02 -0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// instance elements
+const btnPlay = document.getElementById('play-btn');
+const screen = document.getElementById('editor-modal');
+const modalBack = document.querySelector('.modal');
+
+window.addEventListener('click', (event) => {
+  if (event.target === modalBack) {
+    
+  }
+})
